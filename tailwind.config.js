9b6715086c60b1520cff6b6c5758688b7af98450
danmaraba <danielkmaraba@gmail.com>
2024-05-06 19:34:31 +0300

    create and configure tailwind file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'textcolor':'rgba(82,169,33)'
+      }
+    },
+
+  },
+  variants:{},
+  plugins: []
+}
+
